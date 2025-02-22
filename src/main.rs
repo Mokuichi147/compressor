7840b97fece5f92a5ae9c3f47ea8bfae29bf3b70
mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2025-02-22 23:57:07 +0900

    Add command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct AppArgs {
+    #[clap(short, long, default_value = "compress")]
+    output_dir: PathBuf,
+    
+    #[clap(short, long, num_args = 1..)]
+    input_file: Option<Vec<PathBuf>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = AppArgs::parse();
+    println!("{:?}", args.input_file);
 }
